b97bb55ea3f22cc54ffd5e69d5933576d11529a0
javisamlobo <115726044+javisamlobo@users.noreply.github.com>
2022-11-07 18:55:48 +0100

    Primer push

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import com.javier.animales.*;
 public class Main {
     public static void main(String[] args) {
 
+        AnimalPrinter animalPrinter = new AnimalPrinter();
         Dog dog = new Dog();
         dog.setName("Perrete");
         dog.setAge(15);
